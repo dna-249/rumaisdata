5d3c4dea3fb1898e4fdee1c5b7988b6511e5578e
nuraalhaji <you@example.com>
2025-09-01 12:39:37 +0100

    commit

--- app/(auth)/data.tsx
+++ app/(auth)/data.tsx
@@ -4,18 +4,22 @@ import { useEffect, useState } from "react"
 import { FlatList, View } from "react-native"
 
 const Data = ()=>{
-    const [data,setData] = useState("")
+    const [data,setData] = useState()
     useEffect(()=>{
 
-        
+
     const handleRequest =async()=>{
     axios.get("https://dnadata.vercel.app/mtn/api")
-         .then(res =>{setData(res.data); console.log(res.data)}).catch(err => console.log(err))
+         .then(res =>{setData(res.data); console.log(res.data.MTN_DATA)}).catch(err => console.log(err))
     };handleRequest()},[])
     return(
           <View>
            <FlatList numColumns={2} renderItem={(({item}) =>{return(
-            <ThemedText>{item}</ThemedText>
+            <>
+            <ThemedText>{item.size}</ThemedText>
+            <ThemedText>{item.network}</ThemedText>
+            <ThemedText>{item.plan}</ThemedText>
+            </>
             )})} data={data}/>
           </View>
       )
